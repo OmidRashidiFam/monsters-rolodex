c9e169dcb479f79e058260ee1a1f889fc4c50bc6
OmidRashidiFam <omidRashidiFam@gmail.com>
2023-02-11 19:49:45 +0330

    edit header and create the search box component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import SearchBox from './components/search-box/search-box';
+
 class App extends React.Component {
   // initial state
   state = {
     monsters: [],
+    searchFeild: ''
   }
 
   // update the state after mounting app component
@@ -12,15 +15,29 @@ class App extends React.Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState(
+        // updating the state
         () => ({ monsters: users }),
-        () => { console.log('state is:', this.state) } // logging to the console
+        // logging out to the console
+        () => { console.log('state is:', this.state) }
       ))
   }
 
   render() {
+    // destructure methods from component
+    const { onChangeHandler } = this
+
+    // destructure from state
+    const { monsters, searchFeild } = this.state
+
     return (
       <div className='app_container'>
-        <h1>Hello world!</h1>
+        <h1 className='app_title'>Mosters Rolodex</h1>
+        <SearchBox className='searchBox' placeholder='Search Monsters by Name' onChangeHandler={onChangeHandler} />
+        {monsters.map(monster => {
+          return (
+            <h1 key={monster.id}>{monster.name}</h1>
+          )
+        })}
       </div>
     );
   }
